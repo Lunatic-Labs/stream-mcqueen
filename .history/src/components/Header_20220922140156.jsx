a7ba9e0d30671f2a922f6d6521b2f90c06cb2b4a
Paul Diaz <jonesalogan00@gmail.com>
2022-09-22 14:34:30 -0500

    continued  styling css for signup/login page.

---
+++ .history/src/components/Header_20220922140156.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+import Logo from '../assets/Logo.png'
+
+export default function Header() {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+ const Container = styled.div``;
---
+++ .history/src/components/Header_20220922140413.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+import Logo from '../assets/Logo.png'
+
+export default function Header() {
+  return (
+    <Container>
+      <div className="logo">
+        <img src={Logo} alt="Logo" />
+      </div>
+    </Container>
+  )
+}
+ const Container = styled.div``;
