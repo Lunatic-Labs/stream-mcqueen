96a857090631fb41d62920815cb3de24e5f17bbb
Paul Diaz <jonesalogan00@gmail.com>
2022-10-03 10:17:44 -0500

    Sign up styling

--- .history/src/pages/SignUp_20221003101534.jsx
+++ .history/src/pages/SignUp_20221003101534.jsx
@@ -64,24 +64,8 @@ const  Container = styled.div`
                 }
             }
             button {
-                padding: 0.5 rem 1rem;
-                background-color: #F3AA02;
-                border: none;
-                cursor: pointer;
-                color: white;
-                font-weight: bolder;
-                font-size: 1.05 rem;
+
             }
-        }
-        button {
-            padding: 0.5 rem 1rem;
-            background-color: #F3AA02;
-            border: none;
-            cursor: pointer;
-            color: white;
-            border-radius: 0.2 rem;
-            font-weight: bolder;
-            font-size: 1.05 rem;
         }
     }
 `;
