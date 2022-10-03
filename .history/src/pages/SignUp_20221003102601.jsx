39dd9207f8f3463b209d501e2f9807db6aee0432
Paul Diaz <jonesalogan00@gmail.com>
2022-10-03 10:29:12 -0500

    Sign up styling

--- .history/src/pages/SignUp_20221003102601.jsx
+++ .history/src/pages/SignUp_20221003102601.jsx
@@ -6,11 +6,7 @@ import { initializeApp } from 'firebase/app';
 import { useState } from 'react';
 
 export default function SignUp() {
-    const [showPassword, setShowPassword] = useState(false);
-    const [formValues, setFormValues] = useState({
-        email: "",
-        password: "",
-    });
+    const [showPassword, setShowPassword] = useState(false)
   return <Container showPassword={showPassword}>
     <BackgroundImage/>
     <div className="content">
@@ -32,7 +28,7 @@ export default function SignUp() {
                     (<button onClick={()=>setShowPassword(true)}>Sign Up</button>)
             }
         </div>
-        <button>Sign Up</button>
+        <button>Log In</button>
     </div>
     </div>
 
