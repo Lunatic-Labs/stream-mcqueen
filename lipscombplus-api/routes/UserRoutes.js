671a7f16d3486236d4f5427fee7998cc207e2ce5
Paul Diaz <jonesalogan00@gmail.com>
2022-12-06 11:33:23 -0600

    Removed delete liked

--- lipscombplus-api/routes/UserRoutes.js
+++ lipscombplus-api/routes/UserRoutes.js
@@ -1,8 +1,7 @@
-const {addToLikedMedia, getLikedMedia, removeFromLikedMedia} = require("../controllers/UserController")
+const {addToLikedMedia, getLikedMedia} = require("../controllers/UserController")
 const router = require("express").Router();
 
 router.post("/add", addToLikedMedia)
 router.get("/liked/:email", getLikedMedia)
-router.put("/remove", removeFromLikedMedia)
 
 module.exports = router;
